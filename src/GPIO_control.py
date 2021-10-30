5c58b1364e5ef68be88d8cb866f73e1a52985b8b
davidxiong <daweix3@illinois.edu>
2021-10-30 17:57:58 -0500

    update
    finish basic setup parts

--- src/GPIO_control.py
+++ src/GPIO_control.py
@@ -1,1 +1,34 @@
-print('Hello World')
+from PCA9685 import PWM
+import time
+from smbus2 import SMBus
+
+fPWM = 50
+i2c_address = 0x40      #connect to the I2C address of the motor
+channel = 0             # control board address connected to the motor
+a = 8.5
+b = 2
+
+def setup():
+    global pwm
+    bus = SMBus(1)
+    pwm = PWM(bus, i2c_address)
+    pwm.setFreq(fPWM)
+
+
+def setup():
+    global pwm
+    bus = SMBus(1)  # Raspberry Pi revision 2
+    pwm = PWM(bus, i2c_address)
+    pwm.setFreq(fPWM)
+
+
+def setDirection(direction):
+    duty = a / 180 * direction + b
+    pwm.setDuty(channel, duty)
+    print
+    "direction =", direction, "-> duty =", duty
+    time.sleep(1)
+
+
+
+
